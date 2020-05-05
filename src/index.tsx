a313dfa138f232ffdc767366f266afa66aae68a0
Phillip Maier <pmaier983@gmail.com>
2020-05-04 20:58:15 -0400

    Add react-axe
    Care more about a11y.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,12 @@ import ReactDOM from 'react-dom'
 import { ThemeWrapper } from './ThemeWrapper'
 import { App } from './App'
 
+if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line global-require
+  const axe = require('react-axe')
+  axe(React, ReactDOM, 1000)
+}
+
 ReactDOM.render(
   <ThemeWrapper>
     <App />
