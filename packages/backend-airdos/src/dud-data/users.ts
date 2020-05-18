a961987eaf285b837ad0ef4d3abe8aa57ec03447
Phillip Maier <pmaier983@gmail.com>
2020-05-18 11:45:51 -0400

    Add Dud Data to graphql server

---
+++ packages/backend-airdos/src/dud-data/users.ts
@@ -0,0 +1,61 @@
+
+export const users = {
+  gilbirney22: {
+    id: '983',
+    name: 'Gil Birney',
+    firstName: 'Gil',
+    lastName: 'Birney',
+    username: 'gilbirney22',
+    groups: [],
+    collegeName: 'Bowdoin College',
+    followers: [
+    ],
+    following: [
+    ],
+  },
+  pmaier983: {
+    id: '984',
+    name: 'Phillip Maier',
+    firstName: 'Phillip',
+    lastName: 'Maier',
+    username: 'pmaier983',
+    groups: ['Airdos'],
+    collegeName: 'Bowdoin College',
+    followers: [
+      'brock18', 'drewandtom19',
+    ],
+    following: [
+      'brock18',
+    ],
+  },
+  brock18: {
+    id: '985',
+    name: 'William Brockett',
+    firstName: 'William',
+    lastName: 'Brockett',
+    username: 'brock18',
+    groups: ['Airdos'],
+    collegeName: 'Pewter City Battle College',
+    followers: [
+      'drewandtom19', 'pmaier983',
+    ],
+    following: [
+      'pmaier983', 'drewandtom19',
+    ],
+  },
+  drewandtom19: {
+    id: '983',
+    name: 'Andrew Thomson',
+    firstName: 'Andrew',
+    lastName: 'Andrew',
+    username: 'drewandtom19',
+    groups: ['Airdos'],
+    collegeName: 'Brown Academy for Wizards and Witches',
+    followers: [
+      'brock18',
+    ],
+    following: [
+      'brock18', 'pmaier983',
+    ],
+  },
+}
