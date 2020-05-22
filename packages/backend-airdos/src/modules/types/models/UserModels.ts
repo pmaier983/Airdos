0035c6645f6d7a3db5e91899f3f4578459799a5e
Phillip Maier <pmaier983@gmail.com>
2020-05-22 19:04:54 -0400

    Add User Model

---
+++ packages/backend-airdos/src/modules/types/models/UserModels.ts
@@ -0,0 +1,23 @@
+import _ from 'lodash/fp'
+
+// eslint-disable-next-line no-unused-vars
+import { IUserType } from '../UserType'
+import { users } from '../../../dud-data'
+
+const userPaths = ['id', 'name', 'firstName', 'lastName', 'username', 'groups', 'collegeName', 'followers', 'following']
+
+const publicPaths = ['id', 'name', 'firstName', 'lastName', 'username', 'collegeName']
+
+export const getUserModels = ({ user }: {user: IUserType | undefined}) => ({
+  getByUsername: (username: string) => {
+    if (_.get('username', user) === username) {
+      // TODO: have a list of public vs private paths
+      return _.pick(userPaths, users[_.get('username', user)])
+    }
+    if (_.has(username, users)) {
+      const foundUser = users[username]
+      return _.pick(publicPaths, foundUser)
+    }
+    return undefined
+  },
+})
