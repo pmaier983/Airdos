1089e24c90faa7ac2d44203d80cbfd28f9816b80
Phillip Maier <pmaier983@gmail.com>
2020-05-25 15:33:16 -0400

    Move Verify session to the server

--- packages/frontend-airdos/src/hooks/useSession.tsx
+++ packages/frontend-airdos/src/hooks/useSession.tsx
@@ -4,7 +4,6 @@ import { useLocalStorage } from './useLocalStorage'
 type useSessionType = () => [
   undefined | string,
   (username: string) => void,
-  (username: string) => boolean,
   () => void
 ]
 
@@ -14,16 +13,6 @@ const useSession: useSessionType = () => {
   // TODO: ensure that env is present
   const secret = process.env.REACT_APP_REMEMBER_ME_SECRET || '123' // '123' is not the value in the .env
 
-  // TODO: cleanup code. Very tired when I wrote this
-  const verifySession = () => {
-    const sessionInfo = session.split(':')
-    const randomKey = sessionInfo[0]
-    const retrievedHMACKey = sessionInfo[1]
-    // TODO: get username from graphql api
-    const possibleUsername = randomKey
-    return crypto.createHmac('SHA256', secret).update(`${possibleUsername}:${randomKey}`).digest('base64') === retrievedHMACKey
-  }
-
   const establishSession = (username: string) => {
     // const randomKey = crypto.randomBytes(256)
     const randomKey = username
@@ -32,7 +21,7 @@ const useSession: useSessionType = () => {
     setSession(`${randomKey}:${hmacKey}`)
   }
 
-  return [session, establishSession, verifySession, removeSession]
+  return [session, establishSession, removeSession]
 }
 
 export { useSession }
