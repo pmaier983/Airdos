14c2ae7229c83e32e97620e9d0bc2242a97e4152
Phillip Maier <pmaier983@gmail.com>
2020-06-02 20:45:40 -0400

    Update css of SubmitButton

--- packages/frontend-airdos/src/components/buttons/SubmitButton.tsx
+++ packages/frontend-airdos/src/components/buttons/SubmitButton.tsx
@@ -1,17 +1,19 @@
 import React from 'react'
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
 const StyledButton = styled.button`
   border: none;
   cursor: pointer;
-  border-radius: ${({ theme }) => theme.normalBorderRadius};
-  box-shadow: ${({ theme }) => theme.normalHighlightedBoxShadow};
-  font-size: ${({ theme }) => theme.largeFontSize};
-  font-family: ${({ theme }) => theme.normalFontFamily};
-  :hover {
-    background-color: ${({ theme }) => theme.focusColor};
-    color: ${({ theme }) => theme.contrastFocusColor};
-  }
+  ${({ theme }) => css`
+    border-radius: ${theme.normalBorderRadius};
+    box-shadow: ${theme.normalHighlightedBoxShadow};
+    font-size: ${theme.largeFontSize};
+    font-family: ${theme.normalFontFamily};
+    :hover {
+      background-color: ${theme.focusColor};
+      color: ${theme.contrastFocusColor};
+    }
+  `}
 `
 
 const SubmitButton: React.FC = ({ children, ...props }) => (
--- packages/frontend-airdos/src/components/buttons/index.tsx
+++ packages/frontend-airdos/src/components/buttons/index.tsx
@@ -1,3 +1,4 @@
 export { SubmitButton } from './SubmitButton'
 export { FollowButton } from './FollowButton'
 export { MessageButton } from './MessageButton'
+export { AcceptButton } from './AcceptButton'
