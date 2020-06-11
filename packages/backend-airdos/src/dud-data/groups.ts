d2e5101524202377710707717ef9e4ad6ebae2df
Phillip Maier <pmaier983@gmail.com>
2020-06-11 18:38:22 -0400

    add groups and groupsPosts

---
+++ packages/backend-airdos/src/dud-data/groups.ts
@@ -0,0 +1,19 @@
+import { groupsPosts } from './groupsPosts'
+
+export const groups = {
+  Airdos: {
+    name: 'Airdos',
+    members: ['rackraider', 'doctoroz101', 'pmaier983'],
+    posts: groupsPosts.Airdos,
+  },
+  Bowdoin_College: {
+    name: 'Airdos',
+    members: ['rackraider', 'pmaier983'],
+    posts: groupsPosts.Bowdoin_College,
+  },
+  Brown_College: {
+    name: 'Airdos',
+    members: ['doctoroz101'],
+    posts: groupsPosts.Brown_College,
+  },
+}
---
+++ packages/backend-airdos/src/dud-data/groupsPosts.ts
@@ -0,0 +1,26 @@
+export const groupsPosts = {
+  Airdos: [
+    {
+      location: 'Group post',
+      title: 'Dummy Title',
+      postType: 'PUBLICIZING_RESEARCH',
+      text: 'Our latest pre-print, on the use of saliva as a diagnostic sample for testing [link]',
+    },
+  ],
+  Bowdoin_College: [
+    {
+      location: 'Group post',
+      title: 'Dummy Title',
+      postType: 'PUBLICIZING_RESEARCH',
+      text: 'Our latest pre-print, on the use of saliva as a diagnostic sample for testing [link]',
+    },
+  ],
+  Brown_College: [
+    {
+      location: 'Group post',
+      title: 'Dummy Title',
+      postType: 'PUBLICIZING_RESEARCH',
+      text: 'Our latest pre-print, on the use of saliva as a diagnostic sample for testing [link]',
+    },
+  ],
+}
