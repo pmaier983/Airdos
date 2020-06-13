afe72a67c4bb0791264c3f5ab70450c1297eb22e
Phillip Maier <pmaier983@gmail.com>
2020-06-12 21:17:42 -0400

    Add Group Type

---
+++ packages/backend-airdos/src/modules/types/GroupType.ts
@@ -0,0 +1,18 @@
+import { gql } from 'apollo-server-lambda'
+
+export const typeDefs = gql`
+  type Group implements Node {
+    id: ID!
+    name: String
+    posts: [Post]
+    members: [String]
+    admins: [String]
+    private: Boolean
+  }
+`
+
+export const resolvers = {
+  Query: {
+    groupByName: (parent, { name }, context) => context.models.group.getByName(name),
+  },
+}
