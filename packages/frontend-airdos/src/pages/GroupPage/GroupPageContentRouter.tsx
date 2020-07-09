bd7af069c6b2121db5d7cf23d94fc7c6ef5114c9
Phillip Maier <pmaier983@gmail.com>
2020-07-08 21:57:02 -0400

    Add groups Content Router

---
+++ packages/frontend-airdos/src/pages/GroupPage/GroupPageContentRouter.tsx
@@ -0,0 +1,28 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+
+import type { Group } from "../../typings/api"
+
+const GroupPageContentRouter: React.FC<{ group: Group }> = ({
+  group: { name },
+}) => (
+  <>
+    <Route exact path={`/groups/${name}/`}>
+      <Redirect to={`/groups/${name}/feed`} />
+    </Route>
+    <Route exact path={`/groups/${name}/feed`}>
+      Feed
+    </Route>
+    <Route exact path={`/groups/${name}/files`}>
+      Files
+    </Route>
+    <Route exact path={`/groups/${name}/members`}>
+      Members
+    </Route>
+    <Route exact path={`/groups/${name}/settings`}>
+      Settings
+    </Route>
+  </>
+)
+
+export { GroupPageContentRouter }
