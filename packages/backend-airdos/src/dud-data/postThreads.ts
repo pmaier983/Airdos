38e0d3534738c5022690d547cfa8751d8b328f8a
Phillip Maier <pmaier983@gmail.com>
2020-07-13 23:00:23 -0400

    Add basic postThread

---
+++ packages/backend-airdos/src/dud-data/postThreads.ts
@@ -0,0 +1,14 @@
+export const postThreads = {
+  id: 100,
+  group: 'airdos',
+  text: 'Unsupervised Translation of Programming Languages. Feed a model with Python, C++, and Java source code from GitHub, and it automatically learns to translate between the 3 languages in a fully unsupervised way',
+  timeCreated: 1591607880,
+  replies: [
+    {
+      id: 101,
+      group: 'airdos',
+      text: 'We leverage the same principles that we used to translate low-resource languages (https://arxiv.org/abs/1804.07755), i.e. pretraining, denoising auto-encoding, and back-translation. Although initially designed for natural languages, these methods perfectly apply to programming languages.',
+      timeCreated: 1591608000,
+    },
+  ],
+}
