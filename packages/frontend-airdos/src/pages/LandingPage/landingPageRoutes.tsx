ade27a8a93205b2f72c9af8aef7335438fd12b5e
Phillip Maier <pmaier983@gmail.com>
2020-07-18 22:53:51 -0400

    Basic Routes to Post

--- packages/frontend-airdos/src/pages/LandingPage/landingPageRoutes.tsx
+++ packages/frontend-airdos/src/pages/LandingPage/landingPageRoutes.tsx
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom"
 import { PrivateRoute } from "../../components/PrivateRoute"
 import { ProfilePage } from "../ProfilePage"
 import { GroupsRouter } from "../groups/GroupsRouter"
+import { PostPage } from "../PostPage"
 import { LandingPageFeed } from "./LandingPageFeed"
 
 const LandingPageRoutes = () => (
@@ -23,6 +24,9 @@ const LandingPageRoutes = () => (
     <PrivateRoute path="/library">
       <div>This Route has not been built yet</div>
     </PrivateRoute>
+    <Route path="/post">
+      <PostPage />
+    </Route>
   </Switch>
 )
 
--- packages/frontend-airdos/src/pages/PostPage/PostPage.tsx
+++ packages/frontend-airdos/src/pages/PostPage/PostPage.tsx
@@ -1,8 +1,20 @@
 import React from "react"
+import _ from "lodash/fp"
+import { useLocation } from "react-router-dom"
+
+const getUrlPostID = (path: string) => {
+  const splitPath = path.split("/")
+  const profileIndex = _.findIndex(
+    (pathSegment) => pathSegment === "post",
+    splitPath
+  )
+  return splitPath[profileIndex + 1]
+}
 
 const PostPage = () => {
-  console.log("hello")
-  return <div>Gello</div>
+  const { pathname } = useLocation()
+  const postID = getUrlPostID(pathname)
+  return <div>{postID}</div>
 }
 
 export { PostPage }
