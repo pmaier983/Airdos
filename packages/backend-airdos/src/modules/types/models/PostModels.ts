11a1bf93c210fe504989180bdca9a9931d197ea3
Phillip Maier <pmaier983@gmail.com>
2020-08-20 22:20:46 -0400

    Remove unused props

--- packages/backend-airdos/src/modules/types/models/PostModels.ts
+++ packages/backend-airdos/src/modules/types/models/PostModels.ts
@@ -1,11 +1,9 @@
 import _ from 'lodash/fp'
 
 import { posts } from '../../../dud-data'
-import type { User } from '../UserType'
 import type { Post } from '../PostType'
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const getPostModels = ({ user }: {user: User | undefined}) => ({
+export const getPostModels = () => ({
   getPostById: ({ id }) => {
     // eslint-disable-next-line eqeqeq
     const possiblePost = _.find((post: Post) => post.id == id, posts)
