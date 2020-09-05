75cd35dccb66e7c6252ca9e1a541c7df13d0fd3c
Phillip Maier <pmaier983@gmail.com>
2020-09-04 20:19:27 -0400

    Minor Tweak

--- packages/frontend-airdos/src/contexts/CurrentUserProvider.tsx
+++ packages/frontend-airdos/src/contexts/CurrentUserProvider.tsx
@@ -53,7 +53,7 @@ CurrentUserContext.displayName = "CurrentUserContext"
 
 export const useCurrentUserContext = () => useContext(CurrentUserContext)
 
-// TODO: Have a cache do this. Or incognito
+// TODO: Have a cache do this. Or incognito. Use AWS-incognito!
 // This awful. Should have this all in cache.
 const CurrentUserProvider: React.FC = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(undefined)
